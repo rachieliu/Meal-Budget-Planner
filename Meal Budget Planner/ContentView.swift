0e828c62d33b6c1d7fe51a91da04a1ec2c839d27
rachieliu <117690091+rachieliu@users.noreply.github.com>
2024-02-09 14:37:24 -0800

    Initial Commit

---
+++ Meal Budget Planner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meal Budget Planner
+//
+//  Created by Rachel Liu on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meal Budget Planner/Meal_Budget_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_Budget_PlannerApp.swift
+//  Meal Budget Planner
+//
+//  Created by Rachel Liu on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Meal_Budget_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
