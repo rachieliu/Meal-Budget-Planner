06a69c5795839e14ac91298500e24ae49c544c29
oscar <onavarro562@gmail.com>
2024-02-23 21:47:03 -0800

    Addition of Firebase functionality and Users database

---
+++ Meal Budget Planner/Core/Get Started/BudgetSelectViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BudgetSelectViewModel.swift
+//  Meal Budget Planner
+//
+//  Created by Oscar Navarro on 2/23/24.
+//
+
+import Foundation
---
+++ Meal Budget Planner/Core/Get Started/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  Meal Budget Planner
+//
+//  Created by Oscar Navarro on 2/23/24.
+//
+
+import Foundation
